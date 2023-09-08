378d29e0685afa6a580d1583209c25e2dabe6de8
TetsuoOhtaguchi <ohtaguchi1202@gmail.com>
2023-09-08 10:11:10 +0900

    Blogページの記事カードを実装する

--- src/types/NewtType.ts
+++ src/types/NewtType.ts
@@ -1,9 +1,25 @@
 import { Content } from 'newt-client-js'
 
-export type Blog = Content & {
+type Image = {
+  _id: string
+  src: string
+  fileType: string
+  fileName: string
+  fileSize: number
+  width: number
+  height: number
+  title: string
+  altText: string
+  description: string
+  metadata: {}
+}
+
+export type Article = Content & {
   topicTitle: string
-  publishedAt: string
-  updatedAt: string
+  thumbnail: Image
+  postExcerpt: string
   body: string
   slug: string
+  publishedAt: string
+  updatedAt: string
 }

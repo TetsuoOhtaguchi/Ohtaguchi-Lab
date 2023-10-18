8fbff756cb474bb1bd646fde494afdf7b28b4132
TetsuoOhtaguchi <ohtaguchi1202@gmail.com>
2023-10-18 09:23:34 +0900

    アバウトコンテンツ完了

--- src/components/ModalAbout.vue
+++ src/components/ModalAbout.vue
@@ -1,3 +1,53 @@
 <template>
-  <div>ModalAbout.vue</div>
+  <h3 class="modalAbout__h3">About</h3>
+  <div class="modalAbout__itemContainer">
+    <img
+      src="@/assets/images/pngs/noimage.png"
+      alt="noimage画像"
+      class="modalAbout__authorImage"
+    />
+    <p class="modalAboutr__authorText">
+      太田口鉄夫 | Ohtaguchi Tetsuo
+      <br />
+      <br />
+      テストテキストテストテキストテストテキストテストテキスト。<br />
+      テストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキスト。<br />
+      テストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキストテストテキスト。
+    </p>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.modalAbout__h3 {
+  position: absolute;
+  top: 16px;
+  left: 16px;
+  font-size: 18px;
+  font-weight: 600;
+  margin-bottom: 16px;
+}
+
+.modalAbout__itemContainer {
+  display: flex;
+  gap: 32px;
+  max-width: 650px;
+  @media screen and (max-width: 416px) {
+    flex-direction: column;
+  }
+}
+
+.modalAbout__authorImage {
+  background-color: var(--bg-white);
+  height: 175px;
+  width: 175px;
+  object-fit: cover;
+  vertical-align: bottom;
+}
+
+.modalAboutr__authorText {
+  font-size: 12px;
+  text-align: justify;
+  text-justify: inter-ideograph;
+  line-height: 170%;
+}
+</style>
